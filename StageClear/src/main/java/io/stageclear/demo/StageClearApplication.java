c823a34d6f49f538524d2e8a8e527a1a91768177
Ateuo-blip <3145769930@qq.com>
2026-07-04 14:05:17 +0800

    first commit: initial Spring Boot project

---
+++ StageClear/src/main/java/io/stageclear/demo/StageClearApplication.java
@@ -0,0 +1,13 @@
+package io.stageclear.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StageClearApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StageClearApplication.class, args);
+    }
+
+}
