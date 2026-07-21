dbe14fd333aecb49b8a64413411b3be89a5b0982
Ateuo-blip <3145769930@qq.com>
2026-07-21 17:30:58 +0800

    stage7: add agent status and least-busy assignment

--- stageclear-customer-core/src/main/java/io/stageclear/customer/controller/AgentController.java
+++ stageclear-customer-core/src/main/java/io/stageclear/customer/controller/AgentController.java
@@ -10,7 +10,6 @@ import io.stageclear.customer.vo.AgentVO;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
